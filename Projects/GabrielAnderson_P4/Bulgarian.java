c6c1fbcd38bcf70750e6a61c24f6ae3cfd5caf39
Gabriel Anderson <zettablade@gmail.com>
2014-11-24 01:05:34 -0800

    Project 4 source code is complete.

--- Projects/GabrielAnderson_P4/Bulgarian.java
+++ Projects/GabrielAnderson_P4/Bulgarian.java
@@ -1,6 +1,19 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+   An object that simulates a game of bulgarian solitaire.
+   Bulgarian solitaire is played with cards arranged
+   randomly in piles. The game is played by picking
+   up the top card from every pile, and putting those
+   top cards into a pile. This process is repeated until
+   the piles are ordered like "1 2 3..".
+   It's not much fun for a human to play, but computers love it! </br>
+   <p>COPYRIGHT &copy; 2014 Gabriel Anderson. All Rights Reserved.
+   @author Gabriel Anderson
+   @since 23 November 2014
+   @version 1.0
+ */
 public class Bulgarian
 {
    private int soughtPiles;
@@ -11,69 +24,152 @@ public class Bulgarian
    
    private static final int[] EMPTY_PILES = { 0 };
    
+   private static final String GAME_STEP_ECHO_FORMAT
+      = "%d cards. Step %d: %s";
+   private static final String PILE_ECHO_FORMAT
+      = "%d ";
+   private static final String WINNING_PILES_ECHO
+      = "Winning Piles: %s";
+   
+   /**
+      Constructs a Bulgarian object, that plays to build
+      the given number of piles.
+      @param numberOfPiles The number of piles the game will finish with.
+    */
    public Bulgarian(int numberOfPiles)
    {
-      soughtPiles = Math.abs(numberOfPiles);
-      piles = new int[soughtPiles];
-      deckSize = sumOneToN(soughtPiles);
-      steps = 0;
+      // Initializes soughtPiles and deckSize
+      setNumberOfPiles(numberOfPiles);
+      // Initializes steps and piles
+      clear();
    }
    
+   /**
+      Calculates the sum of numbers from 1 to n.
+      @param n The last number in the sequence.
+    */
    private int sumOneToN(int n)
    {
       // Use gauss's trick for adding numbers
       return n * (n + 1) / 2;
    }
    
+   /**
+      Plays a full game of Bulgarian Solitaire.
+      In other words, sorts the piles.
+    */
    public void play()
    {
       createPiles();
       
       while(!winningConfig(piles))
       {
-         playARound(this.piles);
-         removeZeros(this.piles);
-         
-         steps++;
-         
-         System.out.printf("%d cards. Step %d: ", deckSize, steps);
-         printPiles();
-         System.out.println();
+         playTurn();
       }
       
-      System.out.print("Winning Pile: ");
-      printPiles();
+      printWinningPiles();
+   }
+   
+   /**
+      Performs one turn of bulgarian solitaire.
+    */
+   private void playTurn()
+   {
+      playARound(this.piles);
+      printGameStep();
+   }
+   
+   /**
+      Empties all the piles and sets the number of game steps to zero.
+      Use if the game needs to be played again.
+    */
+   public void clear()
+   {
+      piles = EMPTY_PILES;
+      steps = 0;
+   }
+   
+   /**
+      Print deck size, current step, and piles.
+    */
+   public void printGameStep()
+   {
+      System.out.printf(GAME_STEP_ECHO_FORMAT, deckSize, steps, pilesToString());
       System.out.println();
    }
    
-   public void printPiles()
+   /**
+      Prints a message about winning
+      and the current pile configuration.
+      Does not guarentee the pile is a winning pile.
+    */
+   public void printWinningPiles()
    {
-      for(int pile : piles)
-      {
-         System.out.printf("%d ", pile);
-      }
+      System.out.printf(WINNING_PILES_ECHO, pilesToString());
+      System.out.println();
    }
    
+   /**
+      Get the number of steps.
+      @return Number of steps.
+    */
    public int getSteps()
    {
       return steps;
    }
    
+   /**
+      Get the deck size.
+      @return The deck size.
+    */
    public int getDeckSize()
    {
       return deckSize;
    }
    
+   /**
+      Get the number of piles in the winning configuration.
+      @return Number of piles in the winning configuration.
+    */
+   public int getNumberOfPiles()
+   {
+      return soughtPiles;
+   }
+   
+   /**
+      Sets the number of piles in the winning configuration.
+      @param numberOfPiles Number of piles in the winning configuration.
+    */
+   public void setNumberOfPiles(int numberOfPiles)
+   {
+      soughtPiles = Math.abs(numberOfPiles);
+      deckSize = sumOneToN(soughtPiles);
+   }
+   
+   /**
+      Get the current configuration of piles.
+      @return The current configuration of piles.
+    */
    public int[] getPiles()
    {
       return piles;
    }
    
+   /**
+      Set the configuration of piles to something different.
+      @param piles A new pile configuration.
+    */
    public void setPiles(int[] piles)
    {
       this.piles = piles;
    }
    
+   /**
+      Creates a random set of piles. The number of piles
+      is a random number from 1 to the sought piles.
+      The number of cards in each pile is randomly chosen.
+      @return The new set of piles.
+    */
    public int[] createPiles()
    {
       if(soughtPiles == 0)
@@ -83,7 +179,23 @@ public class Bulgarian
       }
       
       int intialNumberOfPiles = generator.nextInt(soughtPiles) + 1;
-      int[] newPiles = new int[intialNumberOfPiles];
+      this.piles = fillWithRandomCards(intialNumberOfPiles);
+      
+      return this.piles;
+   }
+   
+   /**
+      Given the number of piles, randomly places cards into
+      those piles. Some piles may not be given cards.
+      Therefore, this method is not guarenteed to allocate
+      the given number of piles.
+      @param numberOfPiles The number of piles to place cards into.
+      @return Randomly filled set of piles.
+    */
+   private int[] fillWithRandomCards(int numberOfPiles)
+   {
+      int[] newPiles = new int[numberOfPiles];
+      
       // For each card in the deck
       for(int cardCount = 0; cardCount < deckSize; cardCount++)
       {
@@ -94,14 +206,35 @@ public class Bulgarian
          newPiles[currentPile] += 1; 
       }
       
-      newPiles = removeZeros(newPiles);
-      
-      this.piles = newPiles;
-      return newPiles;
+      return removeZeros(newPiles);
    }
    
+   /**
+      Play one round.
+      Decreases all the current piles by one.
+      Adds a new pile that is the size of
+      the number of current piles.
+      Increases the step value.
+      @param piles The piles to play the round with.
+      @return The pile resulting at the end of the round. 
+    */
    public int[] playARound(int[] piles)
-   {  
+   {
+      this.piles = newPileFromTopCards(piles);
+      steps++;
+      
+      return this.piles;
+   }
+   
+   /**
+      Helper method for playARound.
+      Removes a card from the top of each pile,
+      and places those cards into a new pile.
+      @param piles The piles to use.
+      @return A new set of piles.
+    */
+   private int[] newPileFromTopCards(int[] piles)
+   {
       // create the new pile list
       int[] newPiles = new int[piles.length + 1];
       
@@ -113,10 +246,14 @@ public class Bulgarian
       
       newPiles[piles.length] += piles.length;
       
-      this.piles = newPiles;
-      return newPiles;
+      return removeZeros(newPiles);
    }
    
+   /**
+      Remove the zero piles (empty piles).
+      @param piles The piles to clean.
+      @return A new set of piles, with no zeros.
+    */
    private int[] removeZeros (int[] piles)
    {
       if(soughtPiles == 0)
@@ -155,6 +292,11 @@ public class Bulgarian
       return newPiles;
    }
    
+   /**
+      Returns a count of piles with zero cards.
+      @param piles The piles to count.
+      @return The number of piles with zero cards.
+    */
    private int getZeroCount(int[] piles)
    {
       if(soughtPiles == 0)
@@ -173,11 +315,27 @@ public class Bulgarian
       return zeroCount;
    }
    
+   /**
+      Determines if a set of piles is the winning configuration.
+      A winning set of piles is arranged in acending order,
+      with a difference of 1 between each pile size.</br>
+      For examples: 1 2 3 4
+      @param piles The set of piles to check.
+      @return True if winning, false otherwise.
+    */
    public boolean winningConfig(int[] piles)
    {
-      if(soughtPiles == 0)
+      // If there are no piles, then return true
+      if(soughtPiles == 0 && piles.length == 0)
       {
          return true;
+      }
+      
+      // If the length of the piles is not equivalent to
+      // the soughtPiles, then there is no way this is true.
+      if(soughtPiles != piles.length)
+      {
+         return false;
       }
       
       // For each pile
@@ -193,14 +351,48 @@ public class Bulgarian
       
       // The configuration passed the check.
       // This means the piles are winning.
-      
-      // Print out the winning piles.
-      
       return true;
    }
    
+   
+   private static final String SOUGHT_PILES_FORMAT
+      = "Sought piles %d";
+   private static final String DECK_SIZE_FORMAT
+      = "Deck size: %d";
+   private static final String STEPS_FORMAT
+      = "Steps: %d";
+   private static final String PILES_FORMAT
+      = "Piles: %s";
+   
+   /**
+      Get the information about the current game.
+      @return Formatted string with the game's instance variables.
+    */
    public String toString()
    {
-      return "";
+      String output = "";
+      
+      output += String.format(SOUGHT_PILES_FORMAT, soughtPiles);
+      output += String.format(DECK_SIZE_FORMAT, deckSize);
+      output += String.format(STEPS_FORMAT, steps);
+      output += String.format(PILES_FORMAT, pilesToString());
+      
+      
+      return output;
+   }
+   
+   /**
+      Get a string of the current piles.
+      @return String containing current piles.
+    */
+   public String pilesToString()
+   {
+      String pilesString = "";
+      for(int pile : piles)
+      {
+         pilesString += String.format(PILE_ECHO_FORMAT, pile);
+      }
+      
+      return pilesString;
    }
 }
--- Projects/GabrielAnderson_P4/BulgarianTester.java
+++ Projects/GabrielAnderson_P4/BulgarianTester.java
@@ -1,8 +1,152 @@
+import java.util.Scanner;
+
+/**
+   This class if for testing a Bulgarian object.</br>
+   <p>COPYRIGHT &copy; 2014 Gabriel Anderson. All Rights Reserved.
+   @author Gabriel Anderson
+   @since 23 November 2014
+   @version 1.0
+ */
 public class BulgarianTester
 {
+   private static final String ASK_NUM_PILES
+      = "Enter the number of piles (1 or greater): ";
+   private static final String OUT_OF_RANGE_ECHO
+      = "Number needs to be 1 or higher. Try again.";
+   private static final String INVALID_INPUT_ECHO
+      = "Invalid input. Try again.";
+   private static final String GOODBYE_ECHO
+      = "Goodbye.";
+   private static final String CONTINUE_ECHO
+      = "Continue? (yes or no): ";
+      
+   private static final String YES_RESPONSE
+      = "Y";
+   private static final String NO_RESPONSE
+      = "N";  
+   
+   /**
+      The entry point for the tester.
+    */
    public static void main(String[] args)
    {
-      Bulgarian game = new Bulgarian(100);
-      game.play();
+      Bulgarian game = new Bulgarian(0);
+      Scanner in = new Scanner(System.in);
+      boolean isRunning = true;
+      
+      while(isRunning)
+      {
+         int numberOfPiles = askNumberOfPiles(in);
+         game.setNumberOfPiles(numberOfPiles);
+         game.play();
+         game.clear();
+         
+         isRunning = askContinue(in);
+      }
+      
+      sayGoodbye();
+   }
+   
+   /**
+      Asks the user whether to continue or not.
+      Answered with a yes or no.
+      Repeats question until user gives a yes or no.
+      @param in Scanner used to query input.
+      @return True if yes, false if no.
+    */
+   private static boolean askContinue(Scanner in)
+   {
+      while(true)
+      {
+         System.out.print(CONTINUE_ECHO);
+         
+         
+         String response = "";
+         try
+         {
+            response = in.next();
+         }
+         catch(java.util.NoSuchElementException e)
+         {
+            sayGoodbye();
+            System.exit(0);
+         }
+         catch(java.lang.IllegalStateException e)
+         {
+            sayGoodbye();
+            System.exit(0);
+         }
+         
+         response = response.trim().toUpperCase().substring(0,1);
+         
+         if(response.equals(YES_RESPONSE))
+         {
+            return true;
+         }
+         else if(response.equals(NO_RESPONSE))
+         {
+            return false;
+         }
+         
+         // The response didn't pass
+         // the check, was invalid.
+         System.out.println(INVALID_INPUT_ECHO);
+      }
+   }
+   
+   /**
+      Asks the user for the number of piles to play
+      Bulgarian Solitaire with. Repeats question
+      until user gives an input within the proper range.
+      The range is 1 to 2,147,483,647. (can your heap handle it?)
+      @param in The scanner used to query input.
+      @return A valid number of piles.
+    */
+   private static int askNumberOfPiles(Scanner in)
+   {
+      while(true)
+      {
+         System.out.print(ASK_NUM_PILES);
+         
+         // initialize with an invalid value
+         int numberOfPiles = 0;
+         
+         try
+         {
+            numberOfPiles = in.nextInt();
+         }
+         catch(java.util.InputMismatchException e)
+         {
+            System.out.println(INVALID_INPUT_ECHO);
+            in.next();
+         }
+         catch(java.util.NoSuchElementException e)
+         {
+            sayGoodbye();
+            System.exit(0);
+         }
+         catch(java.lang.IllegalStateException e)
+         {
+            sayGoodbye();
+            System.exit(0);
+         }
+         
+         if(numberOfPiles > 0)
+         {
+            return numberOfPiles;
+         }
+         else
+         {
+            System.out.println(OUT_OF_RANGE_ECHO);
+         }
+      }
+   }
+   
+   /**
+      Prints out a goodbye message.
+    */
+   private static void sayGoodbye()
+   {
+      System.out.println(GOODBYE_ECHO);
    }
 }
